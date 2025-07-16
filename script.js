965f05edfad04cadf82b7071df668ef20cb2bf21
Gideon Abel <gideonabel87@gmail.com>
2025-07-16 10:07:00 +0100

    background colour changer finished

---
+++ script.js
@@ -0,0 +1,21 @@
+
+// declare all variables
+const entireBody = document.getElementById("body-container");
+const button = document.getElementById("click-btn");
+
+
+// add event listeners
+button.addEventListener("click", changeColors);
+
+let index = 0;
+
+function changeColors(){
+    let colors = ["red", "blue", "orange", "yellow", "green"];
+    
+    entireBody.style.background = colors[index++];
+
+    // to loop through the array of colors
+    if (index > colors.length - 1){
+        index = 0;
+    }
+}
